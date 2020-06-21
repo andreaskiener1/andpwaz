e12e5f281e72e69b5d464f3b5777ab5dee8fa09b
andreaskiener1 <60392893+andreaskiener1@users.noreply.github.com>
2020-06-21 13:14:45 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -15,7 +15,7 @@ self.addEventListener('install', async e => {
   return self.skipWaiting();
 });
 
-self.addEventListener('acitvate', e => {
+self.addEventListener('activate', e => {
   self.clients.claim();
 });
 
