ae37885561dc83a5e457eb44c1e2947508a689ed
andreaskiener1 <60392893+andreaskiener1@users.noreply.github.com>
2020-06-22 12:59:00 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+
+var express = require('express')
+var path = require('path')
+
+var app = express()
+app.set('port', (process.env.PORT || 5000))
+
+//app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname)));
+
+//var dateipfad = path.join(__dirname, 'public', 'index.html')
+var dateipfad = path.join(__dirname, 'index.html')
+ 
+app.get('/', function(request, response) {
+    response.sendFile(dateipfad)
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
