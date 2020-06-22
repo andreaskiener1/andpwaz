3419493b235fd593cf7c3da02f5670c6444903cd
andreaskiener1 <60392893+andreaskiener1@users.noreply.github.com>
2020-06-22 14:15:06 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 
-var express = require('express')
-var path = require('path')
+var express = require('express');
+var path = require('path');
 
-var app = express()
-app.set('port', (process.env.PORT || 5000))
+var app = express();
+app.set('port', (process.env.PORT || 5000));
 
 //app.use(express.static(path.join(__dirname)));
 app.use(express.static(path.join(__dirname)));
 
 //var dateipfad = path.join(__dirname, 'public', 'index.html')
-var dateipfad = path.join(__dirname, 'index.html')
+var dateipfad = path.join(__dirname, 'index.html');
  
 app.get('/', function(request, response) {
-    response.sendFile(dateipfad)
-})
+    response.sendFile(dateipfad);
+});
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
 
 /*
 const express = require('express');
